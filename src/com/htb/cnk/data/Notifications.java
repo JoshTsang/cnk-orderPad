5be48a2fc76e732055720622691821cbbdcb2d59
Josh <mee990@gmail.com>
2012-08-17 16:09:23 +0800

    updated notification success condition

--- src/com/htb/cnk/data/Notifications.java
+++ src/com/htb/cnk/data/Notifications.java
@@ -79,7 +79,7 @@ public class Notifications {
 
 	public int getNotifiycations() {
 		String notificationPkg = Http.get(Server.GET_NOTIFICATION, null);
-		if (notificationPkg == null) {
+		if (notificationPkg == null || "null".equals(notificationPkg)) {
 			return -1;
 		}
 		try {
