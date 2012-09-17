a4cd829cf8f2dd76fa3e160604511fc3db1929ca
Josh <mee990@gmail.com>
2012-09-17 14:06:12 +0800

    added debug mode to ignore pwd checking

--- src/com/htb/cnk/data/Setting.java
+++ src/com/htb/cnk/data/Setting.java
@@ -6,11 +6,20 @@ import android.content.SharedPreferences.Editor;
 
 public class Setting {
 	private static final String SettingPerferenceName = "setting";
+	private static boolean debug = false;
 	private static SharedPreferences Perference;
 	
 	public Setting(Context context) {
 		Perference = context.getSharedPreferences(SettingPerferenceName,
 			Context.MODE_PRIVATE);
+	}
+	
+	public static void enableDebug(boolean enable) {
+		debug = enable;
+	}
+	
+	public static boolean enabledDebug() {
+		return debug;
 	}
 	
 	public static boolean enabledPersons() {
@@ -27,6 +36,15 @@ public class Setting {
 	
 	public static void enableRingtone(boolean enable) {
 		commitPerference("Ringtone", enable);
+	}
+	
+	
+	public static boolean enabledPWDCheck() {
+		return Perference.getBoolean("pwdCheck", true);
+	}
+	
+	public static void enablePWDCheck(boolean enable) {
+		commitPerference("pwdCheck", enable);
 	}
 	
 	private static void commitPerference(String key, boolean value) {
