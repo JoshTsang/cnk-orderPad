1e01bbfabaf6e1f561b0bff111166449ebab76c2
Jone <likangjing.bobo@gmail.com>
2012-10-29 15:54:30 +0800

    1.add checkbox in ScopeSettingFragment.java

--- src/com/htb/cnk/fragment/ScopeSettingFragment.java
+++ src/com/htb/cnk/fragment/ScopeSettingFragment.java
@@ -12,25 +12,54 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.GridView;
 
 import com.htb.cnk.R;
 import com.htb.cnk.adapter.GridViewImageAdapter;
+import com.htb.cnk.data.Setting;
 
 public class ScopeSettingFragment extends Fragment {
-	GridView gridView_check; 
-	GridViewImageAdapter multiCheck;  
+	GridView gridView_check;
+	GridViewImageAdapter multiCheck;
+	CheckBox scopeCharged;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.gridview, container, false);
-		gridView_check = (GridView) v.findViewById(R.id.gridview);
+		View v = inflater.inflate(R.layout.area_settings, container, false);
+
+		scopeCharged = (CheckBox) v.findViewById(R.id.area);
+		scopeCharged.setOnCheckedChangeListener(scopeCheckedChange);
+		scopeCharged.setChecked(Setting.enableChargedAreaCheckout());
+
+		gridView_check = (GridView) v.findViewById(R.id.areaAridview);
 		multiCheck = new GridViewImageAdapter(getActivity(), true);
 		gridView_check.setAdapter(multiCheck);
 		gridView_check.setOnItemClickListener(gridCheckClickListener());
+		
+		showLoading(Setting.enableChargedAreaCheckout());
 		return v;
 	}
+
+	private void showLoading(boolean bShow) {
+		if (gridView_check == null)
+			return;
+		gridView_check.setVisibility(bShow ? View.VISIBLE : View.GONE);
+	}
+
+	OnCheckedChangeListener scopeCheckedChange = new OnCheckedChangeListener() {
+
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView,
+				boolean isChecked) {
+			Setting.enableChargedAreaAfterCheckout(isChecked);
+			showLoading(isChecked);
+		}
+
+	};
 
 	private OnItemClickListener gridCheckClickListener() {
 		return new OnItemClickListener() {
@@ -39,8 +68,7 @@ public class ScopeSettingFragment extends Fragment {
 					int position, long arg3) {
 				multiCheck.changeState(position);
 				String waiterScope = null;
-				List<Integer> choose = GridViewImageAdapter
-						.getChooseTable();
+				List<Integer> choose = GridViewImageAdapter.getChooseTable();
 				for (int i = 0; i < choose.size(); i++) {
 					if (i == 0) {
 						waiterScope = choose.get(i).toString();
